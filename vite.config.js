bf7278ead5247de2499aa54e08336fa85ff05459
kostyasamsung2013-ship-it <kostyasamsung2013@gmail.com>
2026-08-22 14:19:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+export default defineConfig({
+  plugins: [react()],
+  base: '/electro2/', // Важно: название вашего репозитория со слэшами
+})
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
